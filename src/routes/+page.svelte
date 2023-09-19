08c1c88a33bc85a286009d2fad3f4cc32a8f00b0
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-18 21:15:50 -0700

    Create basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let d = new Date();
+    $: hour = d.getHours();
+    $: minute = d.getMinutes();
+    $: seconds = d.getSeconds();
+    $: displayHour = hour.toString().padStart(2, '0');
+    $: displayMinute = minute.toString().padStart(2, '0');
+    $: displaySeconds = seconds.toString().padStart(2, '0');
+
+    $: if(hour > 12) {
+        hour = hour - 12
+    }
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			d = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<div class="flex h-screen">
+    <div class="m-auto text-center">
+        <h1 class="text-6xl pb-5">It is now:</h1>
+        <h1 class="text-8xl pb-5">{displayHour}:{displayMinute}:{displaySeconds}</h1>
+        <h1 class="text-6xl pb-5">This is going to be a compliment.</h1>
+        <button class="btn">New</button>
+    </div>
+</div>
