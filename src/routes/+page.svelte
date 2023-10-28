f024d2f6293196ee82fe0d6ecb6a371f5c2fd84c
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-28 16:21:59 -0700

    Add text shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,9 +31,9 @@
 
 <div class="flex h-screen bg-cover bg-[url('https://picsum.photos/1920/1080')]">
     <div class="m-auto text-center text-white">
-        <h1 class="text-6xl pb-5">It is now:</h1>
-        <h1 class="text-8xl pb-5">{displayHour}:{displayMinute}:{displaySeconds}</h1>
-        <h1 class="text-6xl pb-8">{compliments[index]}</h1>
+        <h1 class="text-6xl pb-5 [text-shadow:_0_3px_0_rgb(0_0_0_/_40%)]">It is now:</h1>
+        <h1 class="text-8xl pb-5 [text-shadow:_0_3px_0_rgb(0_0_0_/_40%)]">{displayHour}:{displayMinute}:{displaySeconds}</h1>
+        <h1 class="text-6xl pb-8 [text-shadow:_0_3px_0_rgb(0_0_0_/_40%)]">{compliments[index]}</h1>
         <button class="btn" on:click={newCompliment}>New Compliment</button>
     </div>
 </div>
