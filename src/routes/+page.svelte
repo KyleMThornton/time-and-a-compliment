915f89fc9f777619a4c5502c9daa95bc0c0b6c40
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-29 18:46:34 -0700

    Update drop shadow and fix cache issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import { compliments } from '$lib/compliments'
 
     let d = new Date();
+    const timestamp = Date.now();
     $: hour = d.getHours();
     $: minute = d.getMinutes();
     $: seconds = d.getSeconds();
@@ -14,9 +15,17 @@
         hour = hour - 12
     }
 
+    $: bgUrl = '';
+
     let index = Math.floor(Math.random() * compliments.length)
 
     const newCompliment = () => index = Math.floor(Math.random() * compliments.length)
+
+    // const fetchBackground = async () => {
+    //     const res = await fetch('https://picsum.photos/1920/1080')
+    //     bgUrl = res.url
+    //     console.log(bgUrl)
+    // }
 
 	onMount(() => {
 		const interval = setInterval(() => {
@@ -29,11 +38,12 @@
 	});
 </script>
 
-<div class="flex h-screen bg-cover bg-[url('https://picsum.photos/1920/1080?timestamp=' + Date.now())]">
+<div class="flex h-screen bg-cover bg-[url('https://picsum.photos/1920/1080')]">
     <div class="m-auto text-center text-white">
-        <h1 class="text-6xl pb-5" style="text-shadow: 2px 2px 0 #000">It is now:</h1>
-        <h1 class="text-8xl pb-5" style="text-shadow: 2px 2px 0 #000">{displayHour}:{displayMinute}:{displaySeconds}</h1>
-        <h1 class="text-6xl pb-8" style="text-shadow: 2px 2px 0 #000">{compliments[index]}</h1>
+        <h1 class="text-6xl pb-5" style="text-shadow: 3px 2px 0 #000">It is now:</h1>
+        <h1 class="text-8xl pb-5" style="text-shadow: 3px 2px 0 #000">{displayHour}:{displayMinute}:{displaySeconds}</h1>
+        <h1 class="text-6xl pb-8" style="text-shadow: 3px 2px 0 #000">{compliments[index]}</h1>
         <button class="btn" on:click={newCompliment}>New Compliment</button>
+        <!-- <button class="btn" on:click={fetchBackground}>Background</button> -->
     </div>
 </div>
